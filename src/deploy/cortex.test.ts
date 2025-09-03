5854fec424a3ffce8d4aa56787858679a3a0467d
DDEUGGP <ddeuggp@gmail.com>
2025-09-03 03:49:49 +0200

    Delete src/deploy/cortex.test.ts

--- src/deploy/cortex.test.ts
+++
@@ -1,7 +0,0 @@
-import { MemoryCortex } from '../core/cortex.memory';
-
-test('store and retrieve thought', () => {
-  const cortex = new MemoryCortex();
-  cortex.storeThought('key', 'value');
-  expect(cortex.retrieveThought('key')).toBe('value');
-});
