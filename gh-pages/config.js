eadcd8cf4398c5e3eaf15f749a1c2d2f9827a713
DDEUGGP <ddeuggp@gmail.com>
2025-09-03 04:53:29 +0200

    Create config.js

---
+++ gh-pages/config.js
@@ -0,0 +1,11 @@
+export const FIREBASE_CONFIG = {
+  apiKey: 'your-api-key',
+  authDomain: 'your-auth-domain',
+  projectId: 'your-project-id'
+};
+
+export const GLB_PATHS = {
+  neuronnet: '/glb-export/neuronnet.glb',
+  cortex: '/glb-export/cortex.glb',
+  synapse: '/glb-export/synapse.glb'
+};
