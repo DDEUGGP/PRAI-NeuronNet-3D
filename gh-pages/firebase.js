989901278aef972163a716470947bb5cebf34ca6
DDEUGGP <ddeuggp@gmail.com>
2025-09-03 04:33:57 +0200

    Create firebase.js

---
+++ gh-pages/firebase.js
@@ -0,0 +1,14 @@
+📄 firebase.js
+
+`js
+import { initializeApp } from 'firebase/app';
+
+const config = {
+  apiKey: 'your-api-key',
+  authDomain: 'your-auth-domain',
+  projectId: 'your-project-id'
+};
+
+export const app = initializeApp(config);
+`
+
