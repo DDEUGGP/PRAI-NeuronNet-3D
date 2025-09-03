cdad28f774079d55d171f096e467176c34ae1735
DDEUGGP <ddeuggp@gmail.com>
2025-09-03 08:20:46 +0200

    Create package.js

---
+++ .github/workflows/package.js
@@ -0,0 +1,21 @@
+{
+  "name": "prai-neuronnet",
+  "version": "1.0.0",
+  "description": "Matrix-grüne neuronale WebApp für PRAI Yggdrasil 3D-Gehirn",
+  "scripts": {
+    "start": "vite",
+    "build": "vite build",
+    "preview": "vite preview"
+  },
+  "dependencies": {
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0",
+    "three": "^0.158.0",
+    "firebase": "^10.0.0",
+    "react-router-dom": "^6.17.0"
+  },
+  "devDependencies": {
+    "vite": "^5.0.0",
+    "@vitejs/plugin-react": "^4.0.0"
+  }
+}
